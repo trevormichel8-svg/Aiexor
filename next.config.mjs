3c1ae2dd4c519639ac4df4b23d4f41664de14588
trevormichel8-svg <trevormichel8@gmail.com>
2025-12-06 01:00:05 -0700

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true
+  },
+  images: {
+    domains: ["oaidalleapiprodscus.blob.core.windows.net"]
+  }
+};
