ef79752991985ce7df78559a1b96949361c16f45
trevormichel8-svg <trevormichel8@gmail.com>
2025-12-06 00:08:17 -0700

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,1 +1,18 @@
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
+
+export function Card({
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        "glass rounded-xl p-6 border border-white/10 shadow-lg",
+        className
+      )}
+      {...props}
+    />
+  );
+}
 
