7546c00c9cbf4b48faa0e85a069e7aa7cd240566
trevormichel8-svg <trevormichel8@gmail.com>
2025-12-06 00:06:34 -0700

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,1 +1,17 @@
+import { cn } from "@/lib/utils";
+import { InputHTMLAttributes } from "react";
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function Input({ className, ...props }: InputProps) {
+  return (
+    <input
+      className={cn(
+        "w-full px-4 py-2 rounded-md bg-black/30 border border-white/20 text-neutral-200 placeholder-neutral-500 focus:outline-none focus:ring-2 focus:ring-yellow-400",
+        className
+      )}
+      {...props}
+    />
+  );
+}
 
