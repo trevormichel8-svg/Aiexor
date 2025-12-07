37b38812a189effddf87a4dc5ba64b764b998df7
trevormichel8-svg <trevormichel8@gmail.com>
2025-12-07 04:19:23 -0700

    Add Supabase client creation functions

---
+++ lib/supabase.ts
@@ -0,0 +1,14 @@
+import { createBrowserClient, createServerClient } from '@supabase/ssr';
+
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+export const createServerSupabase = (cookies: any) =>
+  createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies }
+  );
